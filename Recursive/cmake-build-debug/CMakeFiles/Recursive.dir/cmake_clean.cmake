d0807bcd8858971cdf19cfee3fd979710477f125
emperorbyl <benjaminprupp@gmail.com>
2017-03-09 21:14:39 -0700

    The source code

---
+++ Recursive/cmake-build-debug/CMakeFiles/Recursive.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Recursive.dir/main.cpp.obj"
+  "Recursive.pdb"
+  "Recursive.exe"
+  "Recursive.exe.manifest"
+  "libRecursive.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Recursive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
